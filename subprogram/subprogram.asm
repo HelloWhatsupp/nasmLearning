bc967307a91690fcf0c7b00dfdb76c83866941c4
HelloWhatsupp <osminkin1998@gmail.com>
2022-05-14 08:41:18 +0400

    added output of string using macros; adding parameters in stack

--- subprogram/subprogram.asm
+++ subprogram/subprogram.asm
@@ -1,3 +1,4 @@
+%include 'stud_io.inc'
 global	_start
 
 section	.bss
@@ -5,11 +6,34 @@ string	resb 256
 
 section	.text
 fill_memory:
+	push	ebp
+	mov	ebp, esp
+	mov	edi, [ebp + 8]
+	mov	ecx, [ebp + 12]
+	mov	al, [ebp + 16]
+.start:
 	mov	[edi], al
 	inc	edi
-	loop	fill_memory
+	loop	.start
+.end:
+	mov	esp, ebp
+	pop	ebp
+	ret
+
+show_str:
+	PUTCHAR	byte [esi]
+	inc	esi
+	loop	show_str
+	PUTCHAR	10
+	ret
 _start:
-	mov	al, '&'
-	mov	ecx, 256
-	mov	edi, string
+	push	'&'
+	push	256
+	push	string
 	call	fill_memory
+	add	esp, 12
+
+	mov	esi, string
+	mov	ecx, 256
+	call	show_str
+	FINISH
