b0d717cc262a608210063d4dc0423aa95fa3fa5d
HelloWhatsupp <osminkin1998@gmail.com>
2022-05-29 11:36:16 +0400

    working macros program. Program count sum of elements

--- pcall9/pcall9.asm
+++ pcall9/pcall9.asm
@@ -1,10 +1,18 @@
 global	_start
 
-%macro	pcall2	3 
+%macro	pcall10	11 
+	push	%11
+	push	%10
+	push	%9
+	push	%8
+	push	%7
+	push	%6
+	push	%5
+	push	%4
 	push	%3
 	push	%2
 	call	%1
-	add	esp, 4 
+	add	esp, 40 
 %endmacro
 %macro	quitCall 2
 	mov	%2, %1
@@ -16,7 +24,7 @@ global	_start
 
 section	.data
 a	equ 7
-val	db	12, 37, 3, 6, 54, 80, 15, 2, 16
+val	db	12, 37, 3, 6, 54, 80, 15, 2, 16, 5
 
 section	.text
 sum:
@@ -28,14 +36,16 @@ sum:
 	xor	ecx, ecx
 	mov	ecx, 8
 .again:
-	mov	ebx, [ebp + 8]		; read value from stack
+	mov	bl,  [ebp + ecx]		; read value from stack
 	add	eax, ebx			; sum value with result
 	add	ecx, 4				; increment on dword value
-	cmp	ecx, 12
-	je	.quit
-	jmp	.again
+	cmp	ecx, 44
+	jle	.again
 .quit:
 	quitCall\
-		ebp, esp	
+		ebp, esp
 _start:
-	pcall2	sum, dword [val], dword [val + 1]
+	pcall10	sum, dword [val], dword [val + 1], dword [val + 2]\
+		, dword [val + 3], dword [val + 4], dword [val + 5]\
+		, dword [val + 6], dword [val + 7], dword [val + 8]\
+		, dword [val + 9]
